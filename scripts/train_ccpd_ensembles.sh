0fe6a384fedd7eaebfab75c96c3560509913f59d
kyle hiroyasu <kylehiroyasu@gmail.com>
2021-09-11 19:30:53 +0200

    correcting ensemble training script

--- scripts/train_ccpd_ensembles.sh
+++ scripts/train_ccpd_ensembles.sh
@@ -1,5 +1,7 @@
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_2 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_3 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_4 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_5 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
+#!/bin/bash
+
+python train.py --data data/ccpd.data --epochs 100 --batch-size 16 --name ccpd_ensemble_1 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 416
+python train.py --data data/ccpd.data --epochs 100 --batch-size 16 --name ccpd_ensemble_2 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 416
+python train.py --data data/ccpd.data --epochs 100 --batch-size 16 --name ccpd_ensemble_3 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 416
+python train.py --data data/ccpd.data --epochs 100 --batch-size 16 --name ccpd_ensemble_4 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 416
+python train.py --data data/ccpd.data --epochs 100 --batch-size 16 --name ccpd_ensemble_5 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 416
