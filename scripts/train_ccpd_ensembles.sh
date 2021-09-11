192f238d5cb3e405f07cf9664b57acff6b7574f2
kyle hiroyasu <kylehiroyasu@gmail.com>
2021-09-11 19:13:47 +0200

    creating shell script to train ensemble

---
+++ scripts/train_ccpd_ensembles.sh
@@ -0,0 +1,5 @@
+python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
+python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_2 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
+python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_3 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
+python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_4 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
+python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_retrain_5 --weights yolov3.pt --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640
