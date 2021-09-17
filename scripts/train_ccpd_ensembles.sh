4e1f77119c95e3317c207bd0192fb9b2bc4a3ada
kyle hiroyasu <kylehiroyasu@gmail.com>
2021-09-17 23:43:07 +0200

    making changes

--- scripts/train_ccpd_ensembles.sh
+++ scripts/train_ccpd_ensembles.sh
@@ -3,5 +3,4 @@
 python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_ensemble_1 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640 --notest
 python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_ensemble_2 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640 --notest
 python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_ensemble_3 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640 --notest
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_ensemble_4 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640 --notest
-python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_ensemble_5 --weights '' --cfg cfg/yolov3-custom-ccpd.cfg --img-size 640 --notest
+python train.py --data data/ccpd.data --epochs 50 --batch-size 16 --name ccpd_mcdrop0 --weights '' --cfg cfg/yolov3-mcdrop0-ccpd.cfg --img-size 640 --notest
